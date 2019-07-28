7551d21393b9bf064c1d98e6c6aec15089ba5564
Bassiuz <b@sdevaan.nl>
2019-07-28 21:06:53 +0200

    Add Cors Support

--- src/main/java/com/bassiuz/meubel/IndexController.java
+++ src/main/java/com/bassiuz/meubel/IndexController.java
@@ -1,13 +1,16 @@
 package com.bassiuz.meubel;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class IndexController {
 
+    
     @GetMapping("/")
     public String index() {
         return "Hello there! I'm running on custom github push.";
